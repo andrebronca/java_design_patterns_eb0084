63feb0dd04fa2e4cc1e9ca06598f43c9c019bec0
André R. B <bronca.andre@gmail.com>
2022-09-16 14:19:28 -0300

    State

---
+++ java_design_patterns_eb0084/src/state/classes/com/state/interfaces/RoboticState.java
@@ -0,0 +1,12 @@
+
+package com.state.interfaces;
+
+/**
+ * 16/set/2022
+ * @author andre
+ */
+public interface RoboticState {
+  public void walk();
+  public void cook();
+  public void off();
+}
---
+++ java_design_patterns_eb0084/src/state/classes/com/state/model/Robot.java
@@ -0,0 +1,27 @@
+package com.state.model;
+
+import com.state.interfaces.RoboticState;
+
+/**
+ * 16/set/2022
+ * @author andre
+ */
+public class Robot implements RoboticState {
+  private 
+  
+  @Override
+  public void walk() {
+    
+  }
+
+  @Override
+  public void cook() {
+    
+  }
+
+  @Override
+  public void off() {
+    
+  }
+  
+}
