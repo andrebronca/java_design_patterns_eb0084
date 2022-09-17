43b2ac83ecd887df4f53c3184141daafb5716853
André R.B. (mac) <bronca.andre@gmail.com>
2022-09-17 11:52:08 -0300

    Corrigindo o problema do interpreter não realizar o cálculo de uma expressão simples

--- java_design_patterns_eb0084/src/interpreter/classes/com/interpreter/app/TestInterpreterPattern.java
+++ java_design_patterns_eb0084/src/interpreter/classes/com/interpreter/app/TestInterpreterPattern.java
@@ -4,29 +4,43 @@ import com.interpreter.implement.ExpressionUtils;
 import com.interpreter.interfaces.Expression;
 import java.util.Stack;
 import com.interpreter.implement.Number;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 16/set/2022
+ *
  * @author andrebronca
+ *
+ * Agora está funcionando, conforme traz no livro não.
+ * Uma ideia de como fazer, bom seria desenvolver expressões complexas. Com 
+ * esse modelo já dá pra pensar. 
  */
 public class TestInterpreterPattern {
+
   public static void main(String[] args) {
+    List<String> operators = new ArrayList<>();
 //    String tokenString = "7 3 - 2 1 + *";
-    String tokenString = "7 - 1";
+    String tokenString = "7 * 2";
     Stack<Expression> stack = new Stack<>();
     String[] tokenArray = tokenString.split(" ");
-    for(String s : tokenArray){
-      if (ExpressionUtils.isOperator(s)){
-        Expression rightExpression = stack.pop();
-        Expression leftExpression = stack.pop();
-        Expression operator = ExpressionUtils.getOperator(s, leftExpression, rightExpression);
-        int result = operator.interpret();
-        stack.push(new Number(result));
-      } else {
+    for (String s : tokenArray) {
+      if (!ExpressionUtils.isOperator(s)) {
         Expression i = new Number(Integer.parseInt(s));
-        stack.push(i);
+        stack.push(i);  //coloca o objeto no top da pilha
+      } else {
+        operators.add(s);
       }
     }
-    System.out.println("( "+ tokenString +" ): "+ stack.pop().interpret());
+
+    for (int i = 0; i < stack.size() - 1; i++) {
+      Expression rightExp = stack.pop();  //remove o último objeto adicionado
+      Expression leftExp = stack.pop();   //remove o último objeto adicionado
+      Expression operator = ExpressionUtils.getOperator(operators.get(i), leftExp, rightExp);
+      int result = operator.interpret();
+      stack.push(new Number(result));
+    }
+
+    System.out.println("( " + tokenString + " ): " + stack.pop().interpret());
   }
 }
